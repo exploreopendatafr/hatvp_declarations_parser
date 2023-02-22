b5d02b0549af5dae29c0114925ba672e571cf506
azro352 <raphi.352@hotmail.fr>
2023-02-22 22:11:05 +0100

    add: setup.py .gitignore requirements.txt

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='hatvp_declarations_parser',
+    version='1.0.0',
+    description='Parser of HATVP declarations file',
+    author="exploreopendatafr",
+    url='https://github.com/exploreopendatafr/hatvp_declarations_parser',
+)
